44e636d2ab947ec9387aeb9b924cf5030a5b87a1
Julia Trinh <juliatrinh2002@gmail.com>
2025-10-24 19:50:56 -0400

    Working sidebar that can close and open
    nothing is inside yet, and the format isn't good

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,17 @@
 
 import './App.css'
 import { Button } from './components/ui/button'
+import Layout from './layout'
 
 function App() {
 
   return (
     <>
+    <Layout>
       <div>
         <Button variant="default">Click me</Button>
       </div>
+    </Layout>
     </>
   )
 }
