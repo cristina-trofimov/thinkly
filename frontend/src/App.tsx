a7db29209ede8fd75135061a3fd454713c043ff7
Boudour Bannouri <131230020+Bou1103@users.noreply.github.com>
2025-10-25 22:13:19 -0400

    add route for /admin

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,21 @@
 
 import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { AdminDashboard } from './components/AdminDashboard'
+import { Button } from './components/ui/button'
 
 function App() {
   return (
-    <>
-      <div className="min-h-screen bg-background">
-        <AdminDashboard />
-      </div>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/admin" element={<AdminDashboard />} />
+        <Route path="/" element={
+          <div>
+            <Button variant="default">Click me</Button>
+          </div>
+        } />
+      </Routes>
+    </Router>
   )
 }
 
