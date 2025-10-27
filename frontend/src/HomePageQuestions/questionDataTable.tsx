509b9b877a510d4a22e23999145061a30adee433
OviyaSinna <oviyacu@gmail.com>
2025-10-26 20:20:26 -0400

    - Added the competition items - Added the table with its search and filter from shadcn

---
+++ frontend/src/HomePageQuestions/questionDataTable.tsx
@@ -0,0 +1,149 @@
+import * as React from "react"
+import { Filter } from "lucide-react";
+import {
+    flexRender,
+    getCoreRowModel,
+    useReactTable,
+    getFilteredRowModel,
+  } from "@tanstack/react-table"
+
+  import type {
+    ColumnDef,
+    ColumnFiltersState
+  } from "@tanstack/react-table"
+
+  import {
+    Table,
+    TableBody,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+  } from "@/components/ui/table"
+  import { Input } from "@/components/ui/input"
+  import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import { Button } from "../components/ui/button"
+   
+  interface DataTableProps<TData, TValue> {
+    columns: ColumnDef<TData, TValue>[]
+    data: TData[]
+  }
+   
+  export function DataTable<TData, TValue>({
+    columns,
+    data,
+  }: DataTableProps<TData, TValue>) {
+    const [columnFilters, setColumnFilters] = React.useState<ColumnFiltersState>(
+        []
+      )
+    
+    const table = useReactTable({
+      data,
+      columns,
+      getCoreRowModel: getCoreRowModel(),
+      onColumnFiltersChange: setColumnFilters,
+      getFilteredRowModel: getFilteredRowModel(),
+      state: {
+        columnFilters,
+      },
+    })
+    
+    return (
+      <div className="overflow-hidden rounded-md ">
+          <div className="flex items-center ">
+        <Input
+          placeholder="Search questions..."
+          value={(table.getColumn("questionTitle")?.getFilterValue() as string) ?? ""}
+          onChange={(event) =>
+            table.getColumn("questionTitle")?.setFilterValue(event.target.value)
+          }
+          className="max-w-sm w-[250px]"
+        />
+        <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+                <Button variant="outline" className="gap-0.5">
+                <Filter className="h-4 w-4 text-primary" />
+                <span className="ml-2 hidden md:inline-flex items-center">
+                    {(table.getColumn("difficulty")?.getFilterValue() as string) ?? "Filter Difficulties"}
+                </span>
+                </Button>
+            </DropdownMenuTrigger>
+
+            <DropdownMenuContent align="start">
+                <DropdownMenuLabel>Filter by Difficulty</DropdownMenuLabel>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem
+                onClick={() => table.getColumn("difficulty")?.setFilterValue(undefined)}
+                >
+                All
+                </DropdownMenuItem>
+                <DropdownMenuItem
+                onClick={() => table.getColumn("difficulty")?.setFilterValue("Easy")}
+                >
+                Easy
+                </DropdownMenuItem>
+                <DropdownMenuItem
+                onClick={() => table.getColumn("difficulty")?.setFilterValue("Medium")}
+                >
+                Medium
+                </DropdownMenuItem>
+                <DropdownMenuItem
+                onClick={() => table.getColumn("difficulty")?.setFilterValue("Hard")}
+                >
+                Hard
+                </DropdownMenuItem>
+            </DropdownMenuContent>
+            </DropdownMenu>
+      </div>
+        <Table>
+          <TableHeader>
+            {table.getHeaderGroups().map((headerGroup) => (
+              <TableRow key={headerGroup.id}>
+                {headerGroup.headers.map((header) => {
+                  return (
+                    <TableHead key={header.id}>
+                      {header.isPlaceholder
+                        ? null
+                        : flexRender(
+                            header.column.columnDef.header,
+                            header.getContext()
+                          )}
+                    </TableHead>
+                  )
+                })}
+              </TableRow>
+            ))}
+          </TableHeader>
+          <TableBody>
+            {table.getRowModel().rows?.length ? (
+              table.getRowModel().rows.map((row) => (
+                <TableRow
+                  key={row.id}
+                  data-state={row.getIsSelected() && "selected"}
+                >
+                  {row.getVisibleCells().map((cell) => (
+                    <TableCell key={cell.id} className="text-left">
+                      {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              ))
+            ) : (
+              <TableRow>
+                <TableCell colSpan={columns.length} className="h-24 text-center">
+                  No results.
+                </TableCell>
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+      </div>
+    )
+  }
