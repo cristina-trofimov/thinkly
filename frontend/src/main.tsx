9fd67283a5ef33f70e1e811a6ef9b352bcc152f9
Midhurshaan <midhur-2002@hotmail.com>
2025-10-27 00:22:08 -0400

    added the page to router. Did not link it to the dashboard since when doing that it was not rendering properly and was instead rendering underneath the dashboard page

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,84 +1,100 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { createBrowserRouter, Navigate, RouterProvider } from 'react-router-dom'
-import './index.css'
-import App from './App.tsx'
-import { Layout } from './components/layout/AppLayout.tsx'
-import { AdminDashboard } from './components/dashboard/AdminDashboard'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import {
+  createBrowserRouter,
+  Navigate,
+  RouterProvider,
+} from "react-router-dom";
+import "./index.css";
+import App from "./App.tsx";
+import { Layout } from "./components/layout/AppLayout.tsx";
+import ManageAccountsPage from "./manage-accounts/ManageAccountsPage.tsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
     handle: {
-      crumb: { title: "Home" }
+      crumb: { title: "Home" },
     },
     children: [
       {
         index: true,
-        element: <Navigate to="/home" replace />
+        element: <Navigate to="/home" replace />,
       },
       {
         path: "home",
-        element: <div>Home Page</div>,        // TO BE REPLACED
+        element: <div>Home Page</div>, // TO BE REPLACED
         handle: {
-          crumb: { title: "Home Page" }
-        }
+          crumb: { title: "Home Page" },
+        },
       },
       {
         path: "algotime",
-        element: <div>AlgoTime</div>,        // TO BE REPLACED
+        element: <div>AlgoTime</div>, // TO BE REPLACED
         handle: {
-          crumb: { title: "AlgoTime" }
-        }
+          crumb: { title: "AlgoTime" },
+        },
       },
       {
         path: "competition",
-        element: <div>Competition</div>,        // TO BE REPLACED
+        element: <div>Competition</div>, // TO BE REPLACED
         handle: {
-          crumb: { title: "Competition" }
-        }
+          crumb: { title: "Competition" },
+        },
       },
       {
         path: "settings",
-        element: <div>Settings</div>,        // TO BE REPLACED
+        element: <div>Settings</div>, // TO BE REPLACED
         handle: {
-          crumb: { title: "Settings" }
-        }
+          crumb: { title: "Settings" },
+        },
       },
       {
         path: "leaderboards",
         handle: {
-          crumb: { title: "Leaderboards" }
+          crumb: { title: "Leaderboards" },
         },
         children: [
           {
             index: true,
-            element: <div>Competitions</div>,        // TO BE REPLACED
+            element: <div>Competitions</div>, // TO BE REPLACED
           },
           {
             path: ":competitionId",
-            element: <div>Competition Leaderboard</div>,        // TO BE REPLACED
+            element: <div>Competition Leaderboard</div>, // TO BE REPLACED
             handle: {
-              crumb: { title: "Competition Leaderboard" }
-            }
-          }
-        ]
+              crumb: { title: "Competition Leaderboard" },
+            },
+          },
+        ],
       },
       {
         path: "dashboard",
-        element: <AdminDashboard />,        
+        element: <div>Admin Dashboard</div>, // TO BE REPLACED
         handle: {
-          crumb: { title: "Admin Dashboard" }
-        }
+          crumb: { title: "Admin Dashboard" },
+        },
       },
-    ]
+      {
+        path: "manage-accounts",
+        element: (
+          <div>
+            Admin Dashboard
+            <ManageAccountsPage />
+          </div>
+        ), // TO BE REPLACED
+        handle: {
+          crumb: { title: "Manage Accounts" },
+        },
+      },
+    ],
   },
-])
+]);
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
     <RouterProvider router={router} />
-  </StrictMode>,
-)
+  </StrictMode>
+);
