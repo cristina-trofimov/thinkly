f0d5dd48b539c4860e78bbc2b3f3c8012b93474a
Midhurshaan <midhur-2002@hotmail.com>
2025-11-09 14:05:38 -0500

    created file for holding manage_accounts related operations

---
+++ backend/src/Components/manage_accounts/manage_accounts_api.py
@@ -0,0 +1,29 @@
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+from DB_Methods.crudOperations import (
+    SessionLocal,
+)
+from models.schema import User, Competition, Scoreboard, UserResult
+
+router = APIRouter()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+@router.get("/manage-accounts")
+def get_all_users(db: Session = Depends(get_db)):
+    users = db.query(User).all()
+    result = []
+    for user in users:
+        result.append({
+            "user_id": user.user_id,
+            "email": user.email,
+            "first_name": user.first_name,
+            "last_name": user.last_name,
+            "type": user.type
+        })
+    return result
