38b71f41e94f0591349cfcee379cc49a53fc8738
Midhurshaan <midhur-2002@hotmail.com>
2025-11-13 21:33:08 -0500

    update linter to allow Tdata in declaration even though it is unused in ManageAccountsDataTable as it is required in theinterface declaration

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -19,5 +19,14 @@ export default defineConfig([
       ecmaVersion: 2020,
       globals: globals.browser,
     },
+    rules: {
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          varsIgnorePattern: "^_",
+          argsIgnorePattern: "^_|^TData"
+        }
+      ]
+    }
   },
 ])
