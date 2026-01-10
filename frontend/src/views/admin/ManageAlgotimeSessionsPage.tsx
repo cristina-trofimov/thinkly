9ba336a262e1a71dc0c235454718ce860ba89385
OviyaSinna <oviyacu@gmail.com>
2026-01-10 01:12:42 -0500

    add a test page after manage algotime card in dashboard

---
+++ frontend/src/views/admin/ManageAlgotimeSessionsPage.tsx
@@ -0,0 +1,10 @@
+const ManageAlgotimeSessionsPage = () => {
+    return (
+      <div>
+        <h1>Hello</h1>
+        <p>This is a test page.</p>
+      </div>
+    );
+  };
+
+export default ManageAlgotimeSessionsPage;
