978815b5a98ebcc851777846f1779de9df0fc190
OviyaSinna <oviyacu@gmail.com>
2026-01-14 23:28:40 -0500

    Create frontend for manage algotime page

--- frontend/src/views/admin/AlgoTimeSession.tsx
+++ frontend/src/views/admin/AlgoTimeSession.tsx
@@ -1,9 +1,18 @@
 import { AlgoTimeSessionForm } from '../../components/forms/AlgoTimeForm';
+import { ArrowLeft } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { useNavigate } from "react-router-dom";
 
 export default function ManageAlgoTimePage() {
+  const navigate = useNavigate();
   return (
+    
     <div className="flex justify-center ">
-    <div className="w-[95%] justify-center shadow border border-primary/30 p-6 rounded-lg flex flex-col gap-4 ">
+      
+    <div className="relative w-[95%] mt-8 justify-center shadow border border-primary/30 p-6 rounded-lg flex flex-col gap-4 ">
+    <Button variant="ghost" size="sm" className="absolute -top-12 left-0 text-muted-foreground" onClick={() => navigate(-1)}>
+              <ArrowLeft className="mr-2 h-4 w-4" /> Back
+            </Button>
       {/* Header */}
       <div className="mb-2">
         <h1 className="text-3xl font-bold text-gray-900 mb-2">
