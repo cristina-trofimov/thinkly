690497b56118c8a7c26bd554cd8c1979c37fb78d
NAO <Naolal30@hotmail.com>
2026-02-03 16:32:04 -0500

    fix linting
    Signed-off-by: NAO <Naolal30@hotmail.com>

--- frontend/src/components/leaderboards/Leaderboards.tsx
+++ frontend/src/components/leaderboards/Leaderboards.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { useState, useEffect } from "react";
-import { Trophy, Clock } from "lucide-react";
 import { CompetitionCard } from "./CompetitionCard";
 import { AlgoTimeCard } from "./AlgoTimeCard";
 import { SearchAndFilterBar } from "./SearchAndFilterBar";
