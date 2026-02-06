76307e4ada3a8b850bad0b5c731e9d0d80cc7409
NAO <Naolal30@hotmail.com>
2026-02-06 11:33:19 -0500

    adjust code so its doesnt break the prod
    Signed-off-by: NAO <Naolal30@hotmail.com>

--- backend/src/main.py
+++ backend/src/main.py
@@ -68,4 +68,4 @@ except AttributeError:
 #  Run server
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0",  port=int(os.getenv("PORT", 8000)), reload=True, reload_excludes=["logs", "*.log", "__pycache__", "./*.db", "./*.sqlite"])
+    uvicorn.run("main:app", host="https://thinkly-production.up.railway.app",  port=int(os.getenv("PORT", 8000)), reload=True, reload_excludes=["logs", "*.log", "__pycache__", "./*.db", "./*.sqlite"])
