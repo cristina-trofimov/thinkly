e1311c1b8d6ae23e60a880f507d485b919d93174
lawrence122 <54451749+lawrence122@users.noreply.github.com>
2026-03-10 13:43:35 -0400

    Update LanguageAPI.tsx

--- frontend/src/api/LanguageAPI.tsx
+++ frontend/src/api/LanguageAPI.tsx
@@ -27,7 +27,7 @@ export async function getLanguageByJudgeID(judge_id: number): Promise<Language> 
     const response = await axiosClient.get<{
       status_code: number,
       data: Language
-    }>(`/lang`, { params: { lang_judge_id: judge_id } })
+    }>(`/lang/${judge_id}`)
 
     return response['data']['data'];
   } catch (err) {
@@ -44,10 +44,38 @@ export async function getLanguageByJudgeID(judge_id: number): Promise<Language> 
 
 export async function AddLanguage(
     lang: Language,
-  ): Promise<Response> {
+  ): Promise<Language> {
     try {
       const response = await axiosClient.post(
         "/langs/add",
+        {
+            lang_judge_id: lang.lang_judge_id,
+            display_name: lang.display_name,
+            monaco_id: lang.monaco_id,
+            active: lang.active,
+        }
+      )
+  
+      return response['data']
+  
+    } catch (err) {
+    logFrontend({
+        level: "ERROR",
+        message: `An error occurred when adding a language. Reason: ${err}`,
+        component: "LanguageAPI",
+        url: globalThis.location.href,
+        stack: (err as Error).stack,
+    })
+      throw err
+    }
+  }
+
+export async function UpdateLanguage(
+    lang: Language,
+  ): Promise<Language> {
+    try {
+      const response = await axiosClient.patch(
+        "/langs/update",
         {
             lang_judge_id: lang.lang_judge_id,
             display_name: lang.display_name,
