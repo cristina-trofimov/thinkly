db35c9084df8040225a1d86754d33daecd27eff6
Constance <knztnce@gmail.com>
2026-03-27 14:21:11 -0400

    fix pretty UI for fail and passed submissions/runs

--- frontend/src/components/codingPage/ConsoleOutput.tsx
+++ frontend/src/components/codingPage/ConsoleOutput.tsx
@@ -5,24 +5,28 @@ const ConsoleOutput = ({logs}: {logs: Judge0Response[]}) => {
     <div data-testid="ConsoleOutput" className='w-full h-full flex flex-col-reverse gap-2 overflow-y-scroll overscroll-x-contain' >
         {logs.map((log, idx) => {
             const stat = log.status.description === "Accepted" ? "Passed" : "Failed"
-            const text_color = log.status.description === "Accepted" ? "bg-green-500" : "bg-red-500"
+            const bgColor =
+              log.status.description === "Accepted" ? "bg-green-100" : "bg-red-100"
+            const textColor =
+              log.status.description === "Accepted" ? "text-green-700" : "text-red-700"
 
             return <div data-testid={`log-${idx+1}`} key={`log-${idx+1}`}
                 className='flex columns-2 gap-1.5 items-start'
             >
-                <div className={`flex flex-col py-0.5 px-1 text-sm ${text_color}`} >
-                    <div>{stat}</div>
-                    {log.time && (
-                         <div data-testid='log-time' >
-                            {log.time} s
-                         </div> 
-                    )}
-
+                <div
+                    className={`flex items-center justify-center py-1 px-2 text-sm font-medium rounded-md ${bgColor} ${textColor}`}
+                >
+                    {stat}
                 </div>
                 <div className={`flex flex-col gap-0.5`} >
                     <div data-testid='log-status' >
                         Status: {log.status.description}
                     </div>
+                    {log.time && (
+                        <div data-testid='log-time' >
+                            {log.time} s
+                        </div> 
+                    )}
                     {log.stdout && (
                         <div data-testid='log-stdout' >
                             stdout: {log.stdout.replaceAll('\n', String.raw`\n`)}
